cd45f68487ceea8d4056e632444b87d90c56576f
Michael Zandi <mzandi@blackberry.com>
2022-09-12 13:53:40 -0400

    Attempt to use more threads & heap to force mmap calls in kotlin test
    I'm very unfamiliar with Kotlin+GraalVM+NativeImage. To try and
    stress test our stack pivot detection in the case of coroutines
    in Kotlin using GraalVM to build a native image, we'll just start with
    making big arrays from an object in a coroutine and hope that triggers mmaps.
    
    This could use much improvement, but is at least a start.

--- test/kotlin-coroutines/app/src/main/kotlin/test/kotlin/coroutines/App.kt
+++ test/kotlin-coroutines/app/src/main/kotlin/test/kotlin/coroutines/App.kt
@@ -12,12 +12,23 @@ class App {
         }
 }
 
+class Junk {
+    fun getItem(i: Int): Int {
+        val data = Array(10000) { i -> i }
+
+        return data[i]
+    }
+}
+
 fun main() = runBlocking {
     println(App().greeting)
 
-    launch {
-        delay(1000)
-        println("example world")
+    for (i in 1..1000) {
+        launch {
+            delay(10)
+            val j = Junk()
+            println("example world " + i + ": " + j.getItem(i))
+        }
     }
     println("example hello")
 }
