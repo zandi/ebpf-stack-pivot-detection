e45cc557a1640c35586dade76cc16c8333d66914
Michael Zandi <mzandi@blackberry.com>
2023-05-12 16:43:44 -0400

    Build container to 'latest' tag
    This is probably basically standard practice. Makes sure we can
    push new images to our registry and `kubernetes rollout restart`
    the deployment to re-start pods with new containers.
    
    Pulling latest containers in kubernetes is handy for dev but bad
    for production. For production we'll need to start building with version
    tags and using those in kubernetes deployment configs.

--- build-container.sh
+++ build-container.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 
+#TAG=`git rev-parse --short --verify HEAD`
+
 # make it easier to build the container with an image name
-docker build -t stack_pivot_poc .
+docker build -t stack_pivot_poc:latest .
